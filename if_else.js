08d4b50a1d3ae6b45acf04060f84de36ef2fe163
Mruzik1 <40868829+Mruzik1@users.noreply.github.com>
2018-10-21 12:36:18 +0300

    Add files via upload

---
+++ if_else.js
@@ -0,0 +1,130 @@
+var a = prompt('Введите число:', '');
+
+// Номер 1
+
+/*
+
+if (a == 0) {
+    alert('True');
+}
+else {
+    alert('False');
+}
+
+*/
+
+// Номер 2
+
+/*
+
+if (a > 0) {
+    alert('True');
+}
+else {
+    alert('False');
+}
+
+*/
+
+// Номер 3
+
+/*
+
+if (a < 0) {
+    alert('True');
+}
+else {
+    alert('False');
+}
+
+*/
+
+// Номер 4
+
+/*
+
+if (a >= 0) {
+    alert('True');
+}
+else {
+    alert('False');
+}
+
+*/
+
+// Номер 5
+
+/*
+
+if (a <= 0) {
+    alert('True');
+}
+else {
+    alert('False');
+}
+
+*/
+
+// Номер 6
+
+/*
+
+if (a != 0) {
+    alert('True');
+}
+else {
+    alert('False');
+}
+
+*/
+
+// Номер 7
+
+/*
+
+if (a == 'test') {
+    alert('True');
+}
+else {
+    alert('False');
+}
+
+*/
+
+// Номер 8
+
+/*
+
+if (a === '1') {
+    alert('True');
+}
+else {
+    alert('False');
+}
+
+*/
+
+// Логические переменные
+
+var test = confirm('Введите true или false');
+
+// Длинная запись
+
+/*
+
+if (test == true) {
+    alert('True');
+}
+else {
+    alert('False');
+}
+
+*/
+
+// Короткая запись
+
+/*
+
+var conf = (test == true) ? alert('True') : alert('False'); 
+
+*/
