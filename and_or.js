83ba0b638709460c1e1951ac66ae89ac889931f8
Mruzik1 <40868829+Mruzik1@users.noreply.github.com>
2018-10-21 12:37:57 +0300

    Add files via upload

---
+++ and_or.js
@@ -0,0 +1,102 @@
+// Переменные
+
+/*
+var a = prompt('Введите значение', '');
+var b = prompt('Введите значение', '');
+var num = prompt('Введите число 1-4', '');
+var result = '';
+var day = prompt('Введите день от 1-31:');
+*/
+
+// Номер 1
+
+/*
+
+if (a > 0 && a < 5) {
+    alert('True');
+}
+else {
+    alert('False');
+}
+
+*/
+
+// Номер 2
+
+/*
+
+if (a == 0 || a == 2) {
+    a = Number(a) + 7;
+}
+else {
+    a = a/10;
+}
+
+alert(a);
+
+*/
+
+// Номер 3
+
+/*
+
+if (a <= 1 && b >= 3) {
+    alert(Number(a) + Number(b));
+}
+else {
+    alert(a - b);
+}
+
+*/
+
+// Номер 4
+
+/*
+
+if (a > 2 && a < 11 || b >= 6 && b < 14) {
+    alert('True');
+}
+else {
+    alert('False');
+}
+
+*/
+
+// Номер 5
+
+/*
+
+switch (num) {
+    case '1': result = alert('winter');
+    break;
+    case '2': result = alert('spring');
+    break;
+    case '3': result = alert('summer');
+    break;
+    case '4': result = alert('autumn');
+    break;
+    default: result = alert('error!');
+    break;
+}
+
+*/
+
+// Номер 6
+
+/*
+
+if (day >= 1 && day <= 10) {
+    alert('Первая.');
+}
+else if (day >= 11 && day <= 20) {
+    alert('Вторая.');
+}
+else if (day >= 21 && day <= 31) {
+    alert('Третья');
+}
+else {
+    alert('Такого дня нет.');
+}
+
+*/
+
